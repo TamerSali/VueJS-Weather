c0b603a3133d6ecf4683ea979911714e3855e583
Tamer Sali <tamerrsali@gmail.com>
2020-06-15 19:16:07 +0300

    added loading algorithm

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ window.onload = function () {
             api_key: "4ed18ee2bc8a7ea550adbd92cb17601a",
             url_base: 'https://api.openweathermap.org/data/2.5/',
             query: "",
-            weather: {},
+            weather: {}, loading: false,
         },
         methods: {
             fetchWeather: function () {
+
                 fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
                     .then(res => {
                         return res.json();
@@ -32,6 +33,13 @@ window.onload = function () {
                 return `${day}, ${date} ${month} ${year}`;
 
             }
+        },
+        created() {
+            this.loading = true;
+            setTimeout(() => {
+                this.fetchWeather();
+                this.loading = false;
+            }, 3000)
         }
 
     });
